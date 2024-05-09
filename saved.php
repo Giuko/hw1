a7f98ab80519c28442b800c3a821ed4f88ad2175
Giuko <peppecicciomaganuco13@gmail.com>
2024-05-09 18:57:29 +0200

    Aggiunto controllo password a livello server

--- saved.php
+++ saved.php
@@ -7,6 +7,21 @@
             $azione = $_POST['azione'];
             $username = mysqli_real_escape_string($conn, $_POST['username']);
             $password = mysqli_real_escape_string($conn, $_POST['password']);
+            
+            //preg_match per fare una ricerca di una espressione regolare
+            // '/' indica inizio e fine dell'espressione regolare
+            $cond_number = preg_match('/\d/', $password);       // '\d' indica carattere numerico
+            // \w indica un qualsiasi carattere alfanumerico, \s indica uno spazio bianco
+            // Con ^ davanti all'espressioni indichiamo qualsiasi carattere che non fa parte ovvero i simboli
+            $cond_symbol = preg_match('/[^\w\s]/', $password);  
+            $cond_capital = preg_match('/[A-Z]/', $password);   // '[A-Z]' corrisponde ad una qualsiasi maiuscola
+            $cond_length = (strlen($password) >= 8);
+
+            $cond = $cond_number & $cond_symbol & $cond_capital & $cond_length;
+            if($cond == 0){
+                exit;
+            }
+
             switch ($azione) {
                 case 'Sign Up':
                     $query = "INSERT INTO `test` VALUES ('$username','$password')";
